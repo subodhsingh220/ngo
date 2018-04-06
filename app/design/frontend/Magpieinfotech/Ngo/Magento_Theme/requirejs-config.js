65c3ddf2dc4308b1ed0c21fad62f8a5257e4e0e7
subodhsingh220 <subodhsingh220@gmail.com>
2018-04-07 00:27:38 +0530

    all changes

---
+++ app/design/frontend/Magpieinfotech/Ngo/Magento_Theme/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+	paths : {
+		'owlslider' : 'Magento_Theme/js/owl.carousel.min'
+	},
+	shim : {
+		'owlslider' :{
+			'deps' : ['jQuery']
+		}
+	}
+};
